665e9ec2fe4cd4b1391465e25df1084db8ee1c91
gabcoelho <gabriela.coelho@zup.com.br>
2020-10-15 21:20:46 -0300

    Initial Commit

---
+++ FaceMaskReminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceMaskReminder
+//
+//  Created by Gabriela Coelho on 15/10/20.
+//  Copyright © 2020 Gabriela Coelho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
